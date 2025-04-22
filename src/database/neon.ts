befa611bd9a11f017dffc4e855431d0ec42e3f78
Mikey75 <Null>
2025-04-23 02:33:31 +1000

    Refactor database connection in neon.ts to streamline import order and ensure consistent usage of Pool from @neondatabase/serverless.

--- src/database/neon.ts
+++ src/database/neon.ts
@@ -1,8 +1,8 @@
 import { env } from "@/lib/env";
+import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import * as schema from "./auth-schema";
 import * as gameSchema from "./game-schema";
-import { Pool } from "@neondatabase/serverless";
 
 const pool = new Pool({ connectionString: env.DATABASE_URL });
 const db = drizzle({ client: pool, schema: { ...schema, ...gameSchema } });
